c920494a814d67f7c4f582fc4bc0bdb446a3bcfa
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2022-08-20 00:38:31 +0500

    First task

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+int num a;
+
+int num b;
+
+Console.Write("Введите число a: ");
+num a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+num b = Convert.ToInt32(Console.ReadLine());
+
+if (num a > num b){
+    Console.WriteLine("Число a большее, число b  меньшее");
+}    
+else{
+    Console.WriteLine(Число b большее, число a меньшее);
+}
+
